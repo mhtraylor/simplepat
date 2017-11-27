33b66114394ab4d39762fd39e27e4b68deab4134
Matthew Traylor <mhtraylor@hotmail.com>
2017-11-26 20:25:18 -0600

    initial commit of project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
-extern crate sdl2;
-extern crate vecmath;
+#![deny(missing_docs)]
+
+//! A simple game engine.
+
+pub extern crate sdl2;
+pub extern crate vecmath;
+
+pub mod core;
+pub mod game;
 
 #[cfg(test)]
 mod tests {
